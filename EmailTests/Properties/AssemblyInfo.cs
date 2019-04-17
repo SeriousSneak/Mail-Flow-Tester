91632bd088219fe206c87f6ffae5261df3a7d8c7
Andrew Stobart <astobart@microsoft.com>
2019-04-17 08:42:16 -0500

    App is now called "Mail Flow Tester" and not "Mail Flow Tester v2"

--- EmailTests/Properties/AssemblyInfo.cs
+++ EmailTests/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mail Flow Tester by Andrew Stobart")]
+[assembly: AssemblyTitle("Mail Flow Tester")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("By Andrew Stobart")]
 [assembly: AssemblyProduct("Mail Flow Tester")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
